ec31a9c54a72a2aeb01c3c57036995505e65ca66
maxime <maximelarquetoux@gmail.com>
2025-12-22 15:05:16 +0100

    Adding pages to the Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
-export default {
-  base: '/nestflux/', // Remplace par le nom exact de ton dépôt GitHub
-}
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/nestflux/', // Indispensable pour GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        // On indique à Vite où sont tous les fichiers HTML
+        main: resolve(__dirname, 'index.html'),
+        signup: resolve(__dirname, 'src/Auth/SignupPage.html'),
+        signin: resolve(__dirname, 'src/Auth/SigninPage.html'),
+        config: resolve(__dirname, 'src/user/Config.html'),
+        profil: resolve(__dirname, 'src/user/Profil.html'),
+      },
+    },
+  },
+})
